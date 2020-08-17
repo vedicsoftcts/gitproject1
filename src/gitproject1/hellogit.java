51b2ab4a15c763305f386cec120e9cc8e656e53c
latif.sunstars@gmail.com <latif.sunstars@gmail.com>
2020-08-17 16:30:54 +0530

    "2nd commit"

--- src/gitproject1/hellogit.java
+++ src/gitproject1/hellogit.java
@@ -5,6 +5,11 @@ public class hellogit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, Git");
+		int a, b,c;
+		a =10;
+		b=20;
+		c = a+b;
+		System.out.println("c = a+b=" + c);
 	}
 
 }
